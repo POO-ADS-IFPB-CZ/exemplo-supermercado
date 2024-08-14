064820fdae2eb3bd34b9f4f610ed303a5f1390ac
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-08-14 07:56:13 -0300

    Modelando a classe supermercado

---
+++ src/model/Supermercado.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Supermercado {
+
+    private String cnpj;
+    private String nome;
+    private Set<Produto> produtos;
+    private Set<Cliente> clientes;
+
+    public Supermercado(String cnpj, String nome) {
+        this.cnpj = cnpj;
+        this.nome = nome;
+        this.produtos = new HashSet<>();
+        this.clientes = new HashSet<>();
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public Set<Cliente> getClientes() {
+        return clientes;
+    }
+
+    @Override
+    public String toString() {
+        return "Supermercado{" +
+                "cnpj='" + cnpj + '\'' +
+                ", nome='" + nome + '\'' +
+                ", produtos=" + produtos +
+                ", clientes=" + clientes +
+                '}';
+    }
+}
