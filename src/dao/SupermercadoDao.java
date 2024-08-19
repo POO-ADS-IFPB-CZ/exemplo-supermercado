dc2557ccc0e1fa42e2585a6884fe209f6ba7ba26
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-08-19 09:47:10 -0300

    Adicionando método para ler arquivo

--- src/dao/SupermercadoDao.java
+++ src/dao/SupermercadoDao.java
@@ -2,8 +2,7 @@ package dao;
 
 import model.Supermercado;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -21,7 +20,28 @@ public class SupermercadoDao {
                 System.out.println("Falha ao criar arquivo");
             }
         }
+    }
 
+    public Set<Supermercado> getSupermercados(){
+        if(arquivo.length()>0){
+            //Há dados no arquivo, ler conjunto
+            try{
+                FileInputStream inputStream = new FileInputStream(arquivo);
+                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+                Set<Supermercado> supermercados =
+                        (Set<Supermercado>) objectInputStream.readObject();
+                return supermercados;
+            } catch (FileNotFoundException e) {
+                System.out.println("Arquivo não encontrado");
+            } catch (IOException e) {
+                System.out.println("Falha ao ler arquivo");
+            } catch (ClassNotFoundException e) {
+                System.out.println("Falha ao ler arquivo");
+            }
+        }else{
+            //Não há nada no arquivo, criar um novo conjunto
+            return new HashSet<>();
+        }
     }
 
     public boolean salvar(Supermercado supermercado) {
