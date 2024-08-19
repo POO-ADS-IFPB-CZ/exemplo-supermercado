9d4042bb479aa65380ff68f9dfcdc19165c9aad7
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-08-19 09:28:51 -0300

    Criando arquivo

--- src/dao/SupermercadoDao.java
+++ src/dao/SupermercadoDao.java
@@ -2,30 +2,44 @@ package dao;
 
 import model.Supermercado;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
 public class SupermercadoDao {
 
-    private Set<Supermercado> supermercados;
+    private File arquivo;
 
     public SupermercadoDao() {
-        supermercados = new HashSet<>();
+        arquivo = new File("supermercados.ser");
+
+        if(!arquivo.exists()){
+            try{
+                arquivo.createNewFile();
+            }catch (IOException e){
+                System.out.println("Falha ao criar arquivo");
+            }
+        }
+
     }
 
     public boolean salvar(Supermercado supermercado) {
-        return supermercados.add(supermercado);
+//        return supermercados.add(supermercado);
+        return false;
     }
 
     public boolean deletar(Supermercado supermercado) {
-        return supermercados.remove(supermercado);
+//        return supermercados.remove(supermercado);
+        return false;
     }
 
     public Supermercado buscarPorCnpj(String cnpj){
-        for(Supermercado supermercado : supermercados){
-            if(supermercado.getCnpj().equals(cnpj)) return supermercado;
-        }
         return null;
+//        for(Supermercado supermercado : supermercados){
+//            if(supermercado.getCnpj().equals(cnpj)) return supermercado;
+//        }
+//        return null;
     }
 
 }
