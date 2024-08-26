39605d32e6dfac52b65e2e2934cc74fe2ee35a1c
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-08-26 10:17:35 -0300

    Adicionando exemplo com JOptionPane no arquivo Main.java

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,22 +1,37 @@
 package view;
 
-import dao.SupermercadoDao;
-import model.Cliente;
-import model.Supermercado;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        SupermercadoDao supermercadoDao = new SupermercadoDao();
+//        JOptionPane.showMessageDialog(null, "Hello world!",
+//                "Mensagem de informação", JOptionPane.INFORMATION_MESSAGE,
+//                icon);
 
-        Supermercado supermercado = new Supermercado("111.111.111/0001-11",
-                "Mercadinho da esquina");
+//        ImageIcon icon = new ImageIcon("img/boneco.png");
+//        String profissoes[] = {"Professor", "Técnico Administrativo", "Terceirizado"};
+//        String retorno = (String) JOptionPane.showInputDialog(null,
+//                "Escolha sua profissão", "Entrada de dados",
+//                JOptionPane.WARNING_MESSAGE, icon, profissoes, profissoes[0]);
+//        System.out.println(retorno);
 
-        if(supermercadoDao.salvar(supermercado)){
-            System.out.println("Salvo com sucesso!");
-        }else{
-            System.out.println("Falha ao salvar");
-        }
+//        ImageIcon icon = new ImageIcon("img/boneco.png");
+//        int retorno = JOptionPane.showConfirmDialog(
+//                null, "Deseja continuar?", "Título",
+//                JOptionPane.YES_NO_OPTION,
+//                JOptionPane.QUESTION_MESSAGE, icon);
+//
+//        if(retorno == JOptionPane.CLOSED_OPTION) {
+//            System.out.println("Não escolheu nada");
+//        }else{
+//            System.out.println("Escolheu alguma coisa");
+//        }
+
+        String profissoes[] = {"Professor", "Técnico Administrativo", "Terceirizado"};
+        JOptionPane.showOptionDialog(null,"Minha mensagem",
+                "Título da mensagem", JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE, null, profissoes, profissoes[0]);
 
     }
 }
