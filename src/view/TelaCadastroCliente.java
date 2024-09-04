61303a458c82d0179f59a5e1db56a131f98d153d
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-09-04 08:14:52 -0300

    Adicionando código para popular a tabela

--- src/view/TelaCadastroCliente.java
+++ src/view/TelaCadastroCliente.java
@@ -1,8 +1,12 @@
 package view;
 
+import model.Cliente;
 import model.Supermercado;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.util.List;
+import java.util.Set;
 
 public class TelaCadastroCliente extends JDialog {
     private JPanel contentPane;
@@ -18,5 +22,28 @@ public class TelaCadastroCliente extends JDialog {
         setModal(true);
         pack();
         getRootPane().setDefaultButton(buttonOK);
+    }
+
+    private void createUIComponents() {
+        table1 = new JTable();
+        preencherTabela();
+    }
+
+    private void preencherTabela() {
+        //TODO: Melhorar o código com lambda e stream
+        String titulos[] = {"CPF", "Nome", "Telefone", "Email"};
+        Set<Cliente> clientes = supermercado.getClientes();
+        List<Cliente> clienteList = clientes.stream().toList();
+        String dados[][] = new String[clientes.size()][4];
+
+        for(int i=0; i<clientes.size(); i++) {
+            dados[i][0] = clienteList.get(i).getCpf();
+            dados[i][1] = clienteList.get(i).getNome();
+            dados[i][2] = clienteList.get(i).getTelefone();
+            dados[i][3] = clienteList.get(i).getEmail();
+        }
+
+        DefaultTableModel model = new DefaultTableModel(dados, titulos);
+        table1.setModel(model);
     }
 }
